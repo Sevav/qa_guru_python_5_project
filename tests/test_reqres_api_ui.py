07aa3b0bfd5e0e37f448853cb3c9d516d014a81e
SegeiVasiltcov <tryaga1n@mail.ri>
2023-07-13 13:10:11 +0300

    init

--- tests/test_reqres_api_ui.py
+++ tests/test_reqres_api_ui.py
@@ -1,5 +1,4 @@
 import allure
-from allure_commons.types import Severity
 from requests import Response
 from selene.support.shared import browser
 from allure import step
@@ -7,106 +6,88 @@ from pages.main import single_resource, user_not_found, register_successful, log
 
 
 @allure.tag("ui")
-@allure.severity(Severity.NORMAL)
 @allure.label('owner', 'Sevav')
-@allure.feature('UI')
 @allure.title('Пользователь не найден')
 def test_single_user_not_found(reqres_ui):
-    """Ответ в случае отсутствия запрашиваемого пользователя"""
-    # GIVEN
+
     with step("SINGLE USER NOT FOUND (API)"):
         response = reqres_ui.get(
-            url='/api/users/23'
+            url='/api/users/26'
         )
-    # WHEN
+
     with step("Go to reqres.in (UI)"):
         browser.open('https://reqres.in/')
-    # THEN
+
     user_not_found(response.status_code)
 
 
 @allure.tag("ui")
-@allure.severity(Severity.NORMAL)
 @allure.label('owner', 'Sevav')
-@allure.feature('UI')
-@allure.title('Один ресурс')
-def test_single_resourse_name(reqres_ui):
-    """Показан один ресурс"""
-    # GIVEN
+@allure.title('Показан один ресурс')
+def test_single_resourse(reqres_ui):
+
     with step("SINGLE <RESOURCE> (API)"):
         response: Response = reqres_ui.get(
             url='/api/unknown/2'
         )
         response_data = response.json()
         response_name = str(response.json()['data']['name'])
-    # WHEN
+
     with step("Go to reqres.in (UI)"):
         browser.open('https://reqres.in')
 
-    # THEN
     single_resource(response_data, response_name)
 
+
 @allure.tag("ui")
-@allure.severity(Severity.NORMAL)
 @allure.label('owner', 'Sevav')
-@allure.feature('UI')
-@allure.title('Успешная регистрация')
-def test_login_successful(reqres_ui):
-    """Успешная регистрация пользователя с получением токена"""
-    # GIVEN
+@allure.title('Регистрация пользователя с получением токена')
+def test_login_successful_with_token(reqres_ui):
+
     with step("REGISTER REQUEST (API)"):
         response: Response = reqres_ui.post(
             url='/api/register',
             data={'email': 'eve.holt@reqres.in', 'password': 'pistol'}
         )
         response_token = str(response.json()['token'])
-    # WHEN
+
     with step("Go to reqres.in (UI)"):
         browser.open('https://reqres.in')
 
-    # THEN
     register_successful(response_token)
 
 
 @allure.tag("ui")
-@allure.severity(Severity.NORMAL)
 @allure.label('owner', 'Sevav')
-@allure.feature('UI')
-@allure.title('Успешная авторизация')
+@allure.title('Успешная авторизация пользователя')
 def test_login_successful(reqres_ui):
-    """Успешная авторизация пользователя"""
-    # GIVEN
+
     with step("LOGIN REQUEST (API)"):
         response: Response = reqres_ui.post(
             url='/api/login',
             data={'email': 'eve.holt@reqres.in', 'password': 'cityslicka'}
         )
         response_token = str(response.json()['token'])
-    # WHEN
+
     with step("Go to reqres.in (UI)"):
         browser.open('https://reqres.in')
 
-    # THEN
     login_successful(response_token)
 
 
 @allure.tag("ui")
-@allure.severity(Severity.NORMAL)
 @allure.label('owner', 'Sevav')
-@allure.feature('UI')
-@allure.title('Ошибка авторизации при входе без пароля')
+@allure.title('Ошибка авторизации если не был введен пароль')
 def test_login_unsuccessful(reqres_ui):
-    """Ошибка авторизации - не введен пароль"""
-    # GIVEN
+
     with step("LOGIN REQUEST (API)"):
         response: Response = reqres_ui.post(
             url='/api/login',
             data={'email': 'peter@klaven'}
         )
         response_error = str(response.json()['error'])
-    # WHEN
+
     with step("Go to reqres.in (UI)"):
         browser.open('https://reqres.in')
 
-    # THEN
     login_unsuccessful(response_error)
